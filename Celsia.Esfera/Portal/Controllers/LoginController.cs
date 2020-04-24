831dc2d8cd85719defcaeba1118ccfa02c556ebc
Diego Alejandro Correa Giraldo <dcorreag@contratista.epm.co>
2020-04-24 00:48:29 -0500

    maquetación inicial

--- Celsia.Esfera/Portal/Controllers/LoginController.cs
+++ Celsia.Esfera/Portal/Controllers/LoginController.cs
@@ -1,16 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Portal.Models;
+using Utilities.Cache;
+using Utilities.Messages;
 
 namespace Portal.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly ILogger<LoginController> logger;
+        private ICacheUtility cache;
+
+        public LoginController(ILogger<LoginController> logger, ICacheUtility cache)
+        {
+            this.logger = logger;
+            this.cache = cache;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return this.View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
     }
 }
