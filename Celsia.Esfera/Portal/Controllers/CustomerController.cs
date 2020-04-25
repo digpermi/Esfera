f55699298fffa433441b753c635202519530c627
Dicnory Jimena Perez Urbano <jimena@luby.com.br>
2020-04-25 17:10:17 -0500

    se agrega la consulta de clientes

--- Celsia.Esfera/Portal/Controllers/CustomerController.cs
+++ Celsia.Esfera/Portal/Controllers/CustomerController.cs
@@ -1,34 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Portal.Models;
+using System.Threading.Tasks;
 
 namespace Portal.Controllers
 {
     public class CustomerController : Controller
     {
+        //private readonly TRepository repository;
+
+        //public CustomerController (TRepository repository)
+        //{
+        //    this.repository = repository;
+        //}
+
         // GET: Customer
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: Customer/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return null;
-            }
 
-            Customer customer = _customer.getCustomerById(id);
+
+        // GET: Customer/Details/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> Details(int id)
+        {
+            // Customer customer = await this.repository.GetAsync(id);
+
+            var customer = new Customer()
+            {
+                Name = "Prueba",
+                LastName = "Prueba1",
+                Identification = "343345656",
+                IdentificationType = new IdentificationType()
+                {
+                    Id = 1,
+                    Name = "cedula"
+                },
+                Address = "calle 58 # 45-09",
+                PhoneNumber = "2020100",
+                CellPhone = "3048064611",
+                Email = "prueba@hoitmail.com",
+                PolicyData = 1,
+                System = new ExternalSystem()
+                {
+                    Id = 1,
+                    Name = "Open"
+                }
+            };
             
             if (customer == null)
             {
-                return null;
+                this.NotFound();
             }
             return View(customer);
         }
