7b1655073771b78950de1a8b4fc660cc278668ca
Diego Alejandro Correa Giraldo <mefiss@gmail.com>
2020-05-18 21:07:15 -0500

    ajustes a la carga de archivos de excel.

--- Celsia.Esfera/Utilities/File/CsvFile.cs
+++ Celsia.Esfera/Utilities/File/CsvFile.cs
@@ -21,12 +21,12 @@ namespace Utilities.File
         {
             CsvParserOptions csvParserOptions = new CsvParserOptions(true, ';');
             CsvParser<TEntity> csvParser = new CsvParser<TEntity>(csvParserOptions, this.csvMapping);
-            ParallelQuery<CsvMappingResult<TEntity>> results = csvParser.ReadFromFile(ruta, Encoding.ASCII);
+            ParallelQuery<CsvMappingResult<TEntity>> results = csvParser.ReadFromFile(ruta, Encoding.UTF8);
 
             var uploadResult = (from result in results
-                                select new { Entity = result.Result, Error = result.Error.Value }).ToList();
+                                select new { Entity = result.Result, Error = result.Error?.Value }).ToList();
 
-            this.Errors = uploadResult.Select(x => x.Error);
+            this.Errors = uploadResult.Where(x => x.Error != null).Select(x => x.Error);
 
             return uploadResult.Select(x => x.Entity);
         }
